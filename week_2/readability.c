9c9e51b2dcfa5a53c1578636b37f7c8773d6d0f6
Capim32 <92652402+Capim32@users.noreply.github.com>
2026-04-30 17:49:09 -0300

    Add readability grade function
    Implement readability grade calculation based on text input.

---
+++ week_2/readability.c
@@ -0,0 +1,53 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+void grade(string text);
+
+int main()
+{
+    string text = get_string("Text: ");
+    grade(text);
+}
+
+void grade(string text)
+{
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == ' ')
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+        {
+            sentences++;
+        }
+    }
+
+    float L = (float) letters / words * 100;
+    float S = (float) sentences / words * 100;
+
+    float index = 0.0588 * L - 0.296 * S - 15.8; // Coleman-Liau index
+
+    if (index < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (index >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %.0f\n", index);
+    }
+}
